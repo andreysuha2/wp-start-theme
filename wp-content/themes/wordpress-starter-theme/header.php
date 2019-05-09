03cf0f53687b638519c931bff4658784675d1255
Andrey Suhenko <andreysuha2@gmail.com>
2019-05-09 15:57:46 +0300

    update start html structure

--- wp-content/themes/wordpress-starter-theme/header.php
+++ wp-content/themes/wordpress-starter-theme/header.php
@@ -5,12 +5,14 @@
 </head>
 
 <body <?php body_class("page-body"); ?>>
+    <div class="wrapper">
+        <div class="content">
+            <header class="page-header">
+                <div class="page-container">
+                    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
 
-	<header class="page-header">
-		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-
-		<nav class="main-nav" role="navigation">
-			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_class' => 'main-nav__list', 'container' => false ) ); ?>
-		</nav>
-
-	</header>
+                    <nav class="main-nav" role="navigation">
+                        <?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_class' => 'main-nav__list', 'container' => false ) ); ?>
+                    </nav>
+                </div>
+            </header>
--- wp-content/themes/wordpress-starter-theme/index.php
+++ wp-content/themes/wordpress-starter-theme/index.php
@@ -1,23 +1,25 @@
 <?php get_header(); ?>
 
 	<main id="main" class="page-main" role="main">
+        <div class="page-container">
 
-		<?php if ( have_posts() ) : ?>
+            <?php if ( have_posts() ) : ?>
 
-			<?php while ( have_posts() ) : the_post(); ?>
+                <?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
+                    <?php get_template_part( 'template-parts/content', get_post_format() ); ?>
 
-			<?php endwhile; ?>
+                <?php endwhile; ?>
 
-			<?php the_posts_navigation(); ?>
+                <?php the_posts_navigation(); ?>
 
-		<?php else : ?>
+            <?php else : ?>
 
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+                <?php get_template_part( 'template-parts/content', 'none' ); ?>
 
-		<?php endif; ?>
+            <?php endif; ?>
 
+        </div>
 	</main>
 
 <?php get_footer(); ?>
