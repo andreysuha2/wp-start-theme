25f3e78e5c83e438b36ec12c50f41674857037e6
Andrey Suhenko <andreysuha2@gmail.com>
2019-05-16 14:09:57 +0300

    ready to test

--- wp-content/themes/wordpress-starter-theme/src/assets/js/index.js
+++ wp-content/themes/wordpress-starter-theme/src/assets/js/index.js
@@ -5,8 +5,3 @@ dl.error("Change git user name, user email, and repo!");
 dl.handler(() => {
     console.log('%cThis notifications show you who use DevLogger))', "font-size: 18px;font-weight: bold");
 });
-console.clear();
-
-let footer = dom.$('a');
-
-console.log(footer);
