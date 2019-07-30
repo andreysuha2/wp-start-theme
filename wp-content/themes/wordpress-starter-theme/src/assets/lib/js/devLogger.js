b971ff32ed4d279f504dfca7b8b786b27f31c7e4
Andrey Suhenko <andreysuha2@gmail.com>
2019-07-30 12:32:58 +0300

    fix devLogger

--- wp-content/themes/wordpress-starter-theme/src/assets/lib/js/devLogger.js
+++ wp-content/themes/wordpress-starter-theme/src/assets/lib/js/devLogger.js
@@ -10,7 +10,7 @@ class DevLogger {
     }
 
     get error() {
-        let args = isDev ? [ console.error(), console, ...arguments ] : [ () => {} ];
+        let args = isDev ? [ console.error, console, ...arguments ] : [ () => {} ];
         return log(args);
     }
 
